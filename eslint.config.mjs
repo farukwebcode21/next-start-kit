738fa721b3892e8320a8155bb77706d9e17354d4
farukwebcode21 <farukahmadmk@yahoo.com>
2024-12-11 22:22:41 +0600

    Fix deployment issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals'),
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
