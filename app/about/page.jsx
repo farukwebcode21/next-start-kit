20efe1ac39fc1c305b9fc733735499b87f3238ba
farukwebcode21 <farukahmadmk@yahoo.com>
2024-12-11 21:59:22 +0600

    home page complete

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from '@/app/about/page.module.css'
+
+const About = () => {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className={style.title}>About Pages</h1>
+      </div>
+    </section>
+  )
+}
+
+export default About
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -15,3 +23,4 @@ export default {
   },
   plugins: [],
 };
+export default tailwindConfig;
